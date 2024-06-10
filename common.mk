4fcfb7bae74eff4574f58812e08d2f7923e2daee
Alucard-Storm <supersayan1000@gmail.com>
2024-06-10 18:12:42 +0000

    sm8350-common: build missing bluetooth libs

--- common.mk
+++ common.mk
@@ -95,7 +95,16 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio-impl
+    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth.audio-impl \
+    com.dsi.ant@1.0.vendor \
+    libbthost_if \
+    libbthost_if.vendor \
+    libldacBT_bco.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
